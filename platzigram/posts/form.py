bf6eeb66eeaa274c7a0b2c720b98a003033b9614
diegoe012 <55567041+dimolinas@users.noreply.github.com>
2021-04-05 18:22:51 -0500

    upload

---
+++ platzigram/posts/form.py
@@ -0,0 +1,12 @@
+#Creando el formulario para post
+
+#django
+from django import forms
+
+from posts.models import Post
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('user','profile','title','photo')
